7579fe9e98e44007e70f62ac629c1751cd4eca73
Larry <git@mail.lambda-larry.net>
2023-04-19 15:12:00 +0000

    feat(neovim)!: Delete neovim config
    The config grew unmanageable

--- .config/nvim/lua/plugins/slime.lua
+++
@@ -1,9 +0,0 @@
-return function()
-  vim.g.slime_paste_file = vim.env.XDG_RUNTIME_DIR .. '/.slime_paste'
-
-  vim.g.slime_target = 'tmux'
-  vim.g.slime_default_config = {
-    socket_name = string.gsub(vim.env['TMUX'], ",.*", ""),
-    target_pane = '{last}',
-  }
-end
