e2a15be0821e78cae56057e3f684e1cc45521014
Larry <git@mail.lambda-larry.net>
2023-04-19 19:23:18 +0000

    feat(xmonad): Cleanup layouts and handle KDE windows

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -6,6 +6,7 @@ import XMonad.Config.Kde
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.StatusBar
 import XMonad.Layout.IM
 import XMonad.Layout.Spacing
@@ -15,6 +16,24 @@ import XMonad.Util.Loggers
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import Data.Ratio ((%))
+import Data.List
+
+
+import Foreign.C.Types (CLong)
+
+-- https://mail.haskell.org/pipermail/xmonad/2014-December/014412.html
+checkAtom :: String -> String -> Query Bool
+checkAtom name value = ask >>= \w -> liftX $ do
+  a   <- getAtom name
+  val <- getAtom value
+  mbr <- getProp w a
+  case mbr of
+    Just r -> return $ elem val $ map fromIntegral r
+    _      -> return False
+
+-- | Helper to read a property
+getProp :: Window -> Atom -> X (Maybe [CLong])
+getProp w a = withDisplay $ \dpy -> io $ getWindowProperty32 dpy a w
 
 
 gaps = spacingRaw True (Border 5 5 5 5) True (Border 5 5 5 5) True
@@ -33,7 +52,7 @@ main =  xmonad $
   , layoutHook         = lessBorders Screen $ gaps $ desktopLayoutModifiers layouts
   }
   where
-    layouts = tiled ||| (reflectHoriz . im_chat $ reflectHoriz tiled) ||| noBorders Full
+    layouts = tiled ||| noBorders Full ||| im_layout tiled ||| reflectHoriz (withIM (225 % 1000) (Role "im-chat") Full)
       where
         -- default tiling algorithm partitions the screen into two panes
         tiled   = Tall nmaster delta ratio
@@ -47,7 +66,9 @@ main =  xmonad $
         -- Percent of screen to increment by when resizing panes
         delta   = 3/100
 
-        im_chat = withIM (225%1000) (Role "im-chat")
+        im_chat = withIM (225 % 1000) (Role "im-chat")
+
+        im_layout = reflectHoriz . im_chat . reflectHoriz
 
 
 keymaps XConfig{modMask = modm} = M.fromList
@@ -83,8 +104,16 @@ keymaps XConfig{modMask = modm} = M.fromList
 
       not = XMonad.Actions.CycleWS.Not
 
-      -- TODO: Manage the window and make it float
-      quitKde = "dbus-send --print-reply --dest=org.kde.ksmserver /KSMServer org.kde.KSMServerInterface.logout int32:1 int32:0 int32:1"
+      quitKde = unwords
+        [ "dbus-send"
+        , "--print-reply"
+        , "--dest=org.kde.ksmserver"
+        , "/KSMServer"
+        , "org.kde.KSMServerInterface.logout"
+        , "int32:1"
+        , "int32:0"
+        , "int32:1"
+        ]
 
 
 dzencmd = "dzen2" ++ dzenFlags
@@ -92,23 +121,32 @@ dzenFlags = " -e onstart=unstick -dock -bg '#282A36' -ta l -fn monospace -h 20 "
 
 
 windowsRules = composeAll
-  -- Remove border from torbrowser to have a normal viewport...
-  [ className =? "Tor Browser"              --> hasBorder False
-  -- torbrowser has to float to have the same viewport as other torbrowser users.
-  , className =? "Tor Browser"              --> doFloat
+  -- Remove border from and float torbrowser to have a normal viewport...
+  [ className =? "Tor Browser"              --> hasBorder False <+> doCenterFloat
   , className =? "kruler"                   --> doFloat
-  , className =? "ksmserver-logout-greeter" --> doFloat
+  , className =? "ksmserver-logout-greeter" --> kdeHook
   , role      =? "About"                    --> doFloat
   , role      =? "Dialog"                   --> doFloat
-  , className =? "plasma"                   --> doFloat
-  , className =? "Plasma"                   --> doFloat
-  , className =? "plasma-desktop"           --> doFloat
-  , className =? "Plasma-desktop"           --> doFloat
-  -- _NET_WM_WINDOW_TYPE_NOTIFICATION
+  , role      =? "PictureInPicture"         --> doFloat
+  , className =? "plasma"                   --> kdeHook
+  , className =? "Plasma"                   --> kdeHook
+  , className =? "plasma-desktop"           --> kdeHook
+  , className =? "Plasma-desktop"           --> kdeHook
+  , checkNotification                       --> kdeHook
+  , appletPopUp                             --> kdeHook
+  , isKDETrayWindow                         --> kdeHook
+  , transience'
   ]
   where
+    checkNotification :: Query Bool
+    checkNotification = checkAtom "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_NOTIFICATION"
+
+    appletPopUp :: Query Bool
+    appletPopUp = checkAtom "_NET_WM_WINDOW_TYPE" "_KDE_NET_WM_WINDOW_TYPE_APPLET_POPUP"
+
     role = stringProperty "WM_WINDOW_ROLE"
-    -- TODO: Make =~ operator for regex or substring
+
+    kdeHook = doIgnore
 
 
 myPP = dzenPP
