ada6de378b12a8cabdb7940ee0c5f5d232ee903a
Larry <git@mail.lambda-larry.net>
2023-04-25 12:29:44 +0000

    feat(neovim): Add border to lspconfig UI

--- .config/nvim/lua/kagutsuchi/lsp.lua
+++ .config/nvim/lua/kagutsuchi/lsp.lua
@@ -5,6 +5,9 @@ function M.setup()
   local mason_lspconfig = require('mason-lspconfig')
   local kagutsuchi_lsp = require('kagutsuchi.lsp.config')
   local lsp_default_config = kagutsuchi_lsp.default
+  require('lspconfig.ui.windows').default_options = {
+    border = 'single',
+  }
 
   for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
     local lsp_config = kagutsuchi_lsp.config(server)
