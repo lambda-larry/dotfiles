8c2bd5ee85fa00cb83fd9d217fcb881dc45a83de
Larry <git@mail.lambda-larry.net>
2023-05-06 15:12:43 +0000

    feat(neovim): Adjust LSP for haskell

--- .config/nvim/ftplugin/haskell.lua
+++ .config/nvim/ftplugin/haskell.lua
@@ -1,8 +1,6 @@
 local ht = require('haskell-tools')
-local on_attach = require('kagutsuchi.lsp.config').default.on_attach
 
 ht.start_or_attach {
-  hls = {
-    on_attach = on_attach,
-  }
+  hls = vim.tbl_extend('force', require('kagutsuchi.lsp.config').default, {
+  }),
 }
