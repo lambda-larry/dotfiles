624247e3553855c9acb1caa255da5f809a8df9eb
Larry <git@mail.lambda-larry.net>
2023-05-13 22:19:47 +0000

    feat(bash): Add bash completion to git aliases

--- .bashrc.d/git.sh
+++ .bashrc.d/git.sh
@@ -22,3 +22,28 @@ if [ "$(type -t __git_ps1)" = function ] && ! [[ $PS1 =~ __git_ps1 ]]; then
 	GIT_PS1_SHOWCOLORHINTS=true
 	PS1="\$(__git_ps1 '[%s] ')$PS1"
 fi
+
+load /usr/share/git/completion/git-completion.bash
+if [ "$(type -t __git_complete)" = function ]; then
+	# Add all git aliases to completion
+	# It's a bit too hard-coded however...
+	for alias in $(alias | cut -d ' ' -f 2 | cut -d = -f 1); do
+		git_alias_complete=''
+		case "$alias" in
+			g)      git_alias_complete=git;;
+			gst)    git_alias_complete=git_status;;
+			ga|ga?) git_alias_complete=git_add;;
+			gd|gd?) git_alias_complete=git_diff;;
+			gm|gm?) git_alias_complete=git_merge;;
+			gd|gd?) git_alias_complete=git_diff;;
+			gc|gcs) git_alias_complete=git_commit;;
+			gc?)    git_alias_complete=git_checkout;;
+
+			*) continue;;
+		esac
+		__git_complete "$alias" "$git_alias_complete"
+	done
+	unset git_alias_complete
+
+	__git_complete dotfile git
+fi
